3258743fc19a0d6d2bd51fe912565ac49669ae30
Pawel <jarrodek@gmail.com>
2019-12-21 13:58:30 -0800

    chore: updating dependencies and tests

--- anypoint-control-mixins.js
+++ anypoint-control-mixins.js
@@ -1,4 +1,3 @@
-import { HoverableMixin } from './hoverable-mixin.js';
-import { ButtonStateMixin } from './button-state-mixin.js';
-import { ControlStateMixin } from './control-state-mixin.js';
-export { HoverableMixin, ButtonStateMixin, ControlStateMixin };
+export { HoverableMixin } from './hoverable-mixin.js';
+export { ButtonStateMixin } from './button-state-mixin.js';
+export { ControlStateMixin } from './control-state-mixin.js';
